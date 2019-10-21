17ffe089865df39b5ea6c5bc658339187b0cfb2f
Rafal Kapusta <rfkapusta@gmail.com>
2019-10-21 20:04:15 +0200

    aded js, very bascic css styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+let allToolTips = document.querySelectorAll('.tooltip');
+// console.log(allToolTips);
+
+allToolTips.forEach(function (tooltip) {
+    tooltip.addEventListener('mouseover', function(e) {
+        let toolTipText = this.dataset.text;
+        let span = document.createElement('span');
+        span.className = 'tooltipText';
+        span.innerText = toolTipText;
+        this.appendChild(span);
+    })
+})
+
+allToolTips.forEach(function (tooltip) {
+    tooltip.addEventListener('mouseout', function(e) {
+        let span = document.querySelector('.tooltipText');
+        this.removeChild(span);
+    })
+})
